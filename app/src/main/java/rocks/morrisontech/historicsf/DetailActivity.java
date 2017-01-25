f65fd02abd0761105dcb7c5ac283af2a87d8d5b9
Craig Morrison <mailforcraig@me.com>
2017-01-24 19:41:01 -0800

    link to .pdf file of historic designation opens browser and downloads or dispays file depending on user's browser. This is not optimal, working on a way to deliver content directly to app. need the text though.

--- app/src/main/java/rocks/morrisontech/historicsf/DetailActivity.java
+++ app/src/main/java/rocks/morrisontech/historicsf/DetailActivity.java
@@ -1,7 +1,10 @@
 package rocks.morrisontech.historicsf;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,7 +13,9 @@ import com.squareup.picasso.Picasso;
 
 import rocks.morrisontech.historicsf.entity.LandmarkEntity;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView designationLinkTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,25 @@ public class DetailActivity extends AppCompatActivity {
         ImageView thumbnailImageView = (ImageView) findViewById(R.id.buildingImageView);
         String imageWebpageUrl = landmarkEntity.getThumbnail();
 
+        TextView yearBuiltTextView = (TextView) findViewById(R.id.year_built);
+        yearBuiltTextView.setText(landmarkEntity.getYear_built());
+
+        TextView architectTextView = (TextView) findViewById(R.id.architect_textView);
+        architectTextView.setText(landmarkEntity.getArchitect());
+
+        designationLinkTextView = (TextView) findViewById(R.id.designationLinkTextView);
+        designationLinkTextView.setText(landmarkEntity.getDesignatio());
+        designationLinkTextView.setOnClickListener(this);
+
+        // set intent to open browser with link
+
+
+
         // parse image url from String of HTML using Jsoup
         String imageSourceUrl = JsoupHelper.getImageSourceUrl(imageWebpageUrl);
 
         // tell user image could not be downloaded
         if (imageSourceUrl == null) {
-            thumbnailImageView.setImageResource(R.drawable.ic_photo_black_48px);
             Toast.makeText(this, "Could not download image", Toast.LENGTH_SHORT).show();
         }
 
@@ -43,4 +61,9 @@ public class DetailActivity extends AppCompatActivity {
                 .into(thumbnailImageView);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(designationLinkTextView.getText())));
+        startActivity(intent);
+    }
 }
